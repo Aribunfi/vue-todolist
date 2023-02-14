0bc898a4ba57e9c176e1cb0303016d8c75e7ba60
Aribunfi <arianna@pabis.de>
2023-02-14 15:08:49 +0100

    lista to do

--- main.js
+++ main.js
@@ -1,2 +1,28 @@
+const { createApp } = Vue;
 
+const app = createApp({
+    data() {
+        return {
+            todoList: [
+                {
+                    text: "Mangiare",
+                    done: true
+                },
+
+                {
+                    text: "Dormire",
+                    done: false
+                },
+
+                {
+                    text: "Giocare",
+                    done: true
+                }
+            ]
+        };
+    },
+    methods: {},
+});
+
+app.mount("#app");
 
